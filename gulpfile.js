fe8f6dfae79122c84c2614289f38ecdf4539f863
Hamirwasia <ssacha@a483e7302e4c.ant.amazon.com>
2019-11-19 00:10:28 -0600

    refactor directories, updated readme and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 "use strict";
 
 // Load plugins
-const browsersync = require("browser-sync").create();
-const del = require("del");
-const gulp = require("gulp");
-const merge = require("merge-stream");
+const browsersync = require('browser-sync').create();
+const del = require('del');
+const gulp = require('gulp');
+const merge = require('merge-stream');
 
 // BrowserSync
 function browserSync(done) {
   browsersync.init({
     server: {
-      baseDir: "./"
+      baseDir: './src'
     },
     port: 3000
   });
@@ -23,42 +23,47 @@ function browserSyncReload(done) {
   done();
 }
 
-// Clean vendor
+// Clean lib
 function clean() {
-  return del(["./vendor/"]);
+  return del(['./src/lib/', './dist']);
 }
 
-// Bring third party dependencies from node_modules into vendor directory
+// Create a dist package
+function dist() {
+  return gulp.src(['src/lib/**/*', 'src/css/**/*', 'src/js/**.*', 'src/index.html'], {base: './src'}).pipe(gulp.dest('./dist')); 
+}
+
+// Bring third party dependencies from node_modules into lib directory
 function modules() {
   // Bootstrap
   var bootstrap = gulp.src('./node_modules/bootstrap/dist/**/*')
-    .pipe(gulp.dest('./vendor/bootstrap'));
+    .pipe(gulp.dest('./src/lib/bootstrap'));
   // jQuery
   var jquery = gulp.src([
       './node_modules/jquery/dist/*',
       '!./node_modules/jquery/dist/core.js'
     ])
-    .pipe(gulp.dest('./vendor/jquery'));
+    .pipe(gulp.dest('./src/lib/jquery'));
   // jQuery Easing
   var jqueryEasing = gulp.src('./node_modules/jquery.easing/*.js')
-    .pipe(gulp.dest('./vendor/jquery-easing'));
+    .pipe(gulp.dest('./src/lib/jquery-easing'));
   return merge(bootstrap, jquery, jqueryEasing);
 }
 
 // Watch files
 function watchFiles() {
-  gulp.watch("./**/*.css", browserSyncReload);
-  gulp.watch("./**/*.html", browserSyncReload);
+  gulp.watch('./src/**/*.css', browserSyncReload);
+  gulp.watch('./src/**/*.html', browserSyncReload);
 }
 
 // Define complex tasks
-const vendor = gulp.series(clean, modules);
-const build = gulp.series(vendor);
+const lib = gulp.series(clean, modules);
+const build = gulp.series(lib, dist);
 const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
 
 // Export tasks
 exports.clean = clean;
-exports.vendor = vendor;
+exports.lib = lib;
 exports.build = build;
 exports.watch = watch;
 exports.default = build;
